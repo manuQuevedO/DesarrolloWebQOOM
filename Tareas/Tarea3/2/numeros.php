3245c64b2bd4a62f837938eb1f7f005ef3dd1ec2
Manuel Quevedo <oscarmanuel7612@gmail.com>
2023-08-23 20:16:51 -0400

    .

--- Tareas/Tarea3/2/numeros.php
+++ Tareas/Tarea3/2/numeros.php
@@ -3,14 +3,14 @@ $numeros = range(1, 20);
 $numerosPares = array();
 $numerosImpares = array();
 
-foreach ($numeros as $numero) {
-    if ($numero % 2 == 0) {
-        $numerosPares[] = $numero;
-    } else {
-        $numerosImpares[] = $numero;
+foreach ($numeros as $numero) { // Iteramos sobre todos los números
+    if ($numero % 2 == 0) { // Si el número es par
+        $numerosPares[] = $numero; // Lo añadimos al array de números pares
+    } else { // Si no
+        $numerosImpares[] = $numero; // Lo añadimos al array de números impares
     }
 }
 
-echo "Números pares: " . implode(", ", $numerosPares) . "<br>";
+echo "Números pares: " . implode(", ", $numerosPares) . "<hr>";
 echo "Números impares: " . implode(", ", $numerosImpares);
 ?>
--- Tareas/Tarea3/3/dibujar.php
+++ Tareas/Tarea3/3/dibujar.php
@@ -1,23 +1,28 @@
 <?php
+
+// Si se ha enviado el formulario
 if(isset($_GET['numero'])) {
+
     $numero = intval($_GET['numero']);
 
+    echo "<table border='1'>";
     echo "<style>";
     echo "table { border-collapse: collapse; }";
-    echo "td { width: 30px; height: 30px; text-align: center; }";
+    echo "td { width: 80px; height: 80px; }";
     echo "</style>";
+    
 
-    echo "<table border='1'>";
     for ($fila = 0; $fila < $numero; $fila++) {
         echo "<tr>";
+
         for ($col = 0; $col < $numero; $col++) {
-            $color = ($fila + $col) % 2 == 0 ? 'white' : 'black';
+            $color = ($fila + $col) % 2 == 0 ? 'white' : 'yellow';
             echo "<td style='background-color: $color;'></td>";
         }
+
         echo "</tr>";
     }
+
     echo "</table>";
-} else {
-    echo "Por favor, ingrese un número válido.";
-}
-?>
+} 
+
