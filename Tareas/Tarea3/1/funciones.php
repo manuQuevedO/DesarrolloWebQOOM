357bddba01cc228eab8ef3cdcdc40e95dee1b368
Manuel Quevedo <oscarmanuel7612@gmail.com>
2023-08-23 20:58:17 -0400

    .

--- Tareas/Tarea3/1/funciones.php
+++ Tareas/Tarea3/1/funciones.php
@@ -1,20 +1,15 @@
-<!-- funciones.php -->
+
 <?php
-// 1. Se divide la cadena en palabras
 function PalabraMasLarga($cadena) {
     $palabras = explode(" ", $cadena);
     $palabraMasLarga = "";
     
-    // 2. Se recorren las palabras
     foreach ($palabras as $palabra) {
-        // 3. Se verifica si la longitud de la palabra es mayor que la longitud de la palabra mas larga
         if (strlen($palabra) > strlen($palabraMasLarga)) {
-            // 4. Se reemplaza la palabra mas larga
             $palabraMasLarga = $palabra;
         }
     }
     
-    // 5. Se retorna la palabra mas larga
     return $palabraMasLarga;
 }
 ?>
--- Tareas/Tarea3/2/numeros.php
+++ Tareas/Tarea3/2/numeros.php
@@ -3,11 +3,11 @@ $numeros = range(1, 20);
 $numerosPares = array();
 $numerosImpares = array();
 
-foreach ($numeros as $numero) { // Iteramos sobre todos los números
-    if ($numero % 2 == 0) { // Si el número es par
-        $numerosPares[] = $numero; // Lo añadimos al array de números pares
-    } else { // Si no
-        $numerosImpares[] = $numero; // Lo añadimos al array de números impares
+foreach ($numeros as $numero) {
+    if ($numero % 2 == 0) {
+        $numerosPares[] = $numero;
+    } else {
+        $numerosImpares[] = $numero;
     }
 }
 
